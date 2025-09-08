941928474b7d5bd18d2352463e24a36be6774ec4
Andres Coseani <andycoseani.16@gmail.com>
2025-09-08 11:49:05 -0300

    Actualizar Pantano

--- PrimerClaseVirtual/src/primerclasevirtual/Pantano.java
+++ PrimerClaseVirtual/src/primerclasevirtual/Pantano.java
@@ -8,6 +8,22 @@ package primerclasevirtual;
  *
  * @author Alumno
  */
-public interface Pantano {
-    
+public interface Pantano { 
+    void CrecerVegetacion();
+
+    default void reaccionaconClima(String Clima, int Velocidad, int camuflaje) {
+        if (Clima.equalsIgnoreCase("Lluvia")) {
+            Velocidad -= 20;
+            System.out.println("El pantano se inunda, la velocidad baja a " + Velocidad);
+            
+            CrecerVegetacion();
+            camuflaje =+ 15;
+            System.out.println("La vegetación crece y el camuflaje sube a " + camuflaje);
+            
+        } else if (Clima.equalsIgnoreCase("Seco")) {
+            System.out.println("El pantano se seca, la vegetación deja de crecer y el camuflaje baja.");
+        } else {
+            System.out.println("El clima no afecta al pantano.");
+        }
+    }
 }
