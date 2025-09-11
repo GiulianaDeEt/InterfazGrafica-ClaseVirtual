be543e1ed0550e5567eebf8de646a5ec23122a51
lucasvalinottiIES <164922438+lucasvalinottiIES@users.noreply.github.com>
2025-09-11 11:18:31 -0300

    ADD interface SelvaTropical

---
+++ PrimerClaseVirtual/src/primerclasevirtual/SelvaTropical.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package primerclasevirtual;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface SelvaTropical {
+    void desplazarse(String lugar);
+    void alimentarse();
+    void emitirSonido();
+    boolean puedeHablar();
+}
