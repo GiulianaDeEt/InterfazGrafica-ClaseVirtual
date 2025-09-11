4b3b8a3454c53157833d5c546ace2d3466431803
lucasvalinottiIES <164922438+lucasvalinottiIES@users.noreply.github.com>
2025-09-11 12:34:53 -0300

    FIX clase main, chequeo con instanceOf

--- PrimerClaseVirtual/src/primerclasevirtual/PrimerClaseVirtual.java
+++ PrimerClaseVirtual/src/primerclasevirtual/PrimerClaseVirtual.java
@@ -5,26 +5,29 @@
 package primerclasevirtual;
 
 public class PrimerClaseVirtual {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         System.out.println("~ECOSISTEMA VIRTUAL~");
-        
+
         // Creando animales con diferentes capacidades
         Elefante dumbo = new Elefante("Dumbo", 15, 4500.0, 1.8, true);
-        
+
         MonoAullador titi = new MonoAullador("Titi", 3, 14.4, "Alouatta caraya");
         MonoAullador moncholo = new MonoAullador("Moncholo", 5, 16.2, "Alouatta caraya");
         MonoAullador peporro = new MonoAullador("Peporro", 3, 15.0, "Alouatta caraya");
-        
+
         titi.comer();
         titi.hacerSonido();
         titi.moverse();
         System.out.println(titi.getTipoHabitat());
-        
-        titi.cazar(dumbo);
-        titi.protegerseDeDepredador(dumbo);
-        titi.descansar();
-        titi.competirPorAlimento(moncholo);
-        titi.compartirAlimento(peporro);
-        
+
+        if (titi instanceof SelvaTropical) {
+            titi.cazar(dumbo);
+            titi.protegerseDeDepredador(dumbo);
+            titi.descansar();
+            titi.competirPorAlimento(moncholo);
+            titi.compartirAlimento(peporro);
+        }
+
     }
 }
