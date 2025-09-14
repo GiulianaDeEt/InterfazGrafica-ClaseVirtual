2946e4d69b7bf7813de74db1c3df795797f352f1
lucasvalinottiIES <164922438+lucasvalinottiIES@users.noreply.github.com>
2025-09-14 11:39:17 -0300

    MODIFY clase main

--- PrimerClaseVirtual/src/primerclasevirtual/PrimerClaseVirtual.java
+++ PrimerClaseVirtual/src/primerclasevirtual/PrimerClaseVirtual.java
@@ -9,10 +9,12 @@ public class PrimerClaseVirtual {
     public static void main(String[] args) {
         System.out.println("~ECOSISTEMA VIRTUAL~");
 
-        // Creando animales con diferentes capacidades
-        Elefante dumbo = new Elefante("Dumbo", 15, 4500.0, 1.8, true);
-
         MonoAullador titi = new MonoAullador("Titi", 3, 14.4, "Alouatta caraya");
+        Perezoso perez = new Perezoso("Perez", 5, 43, "");
+        Guacamayo lolo = new Guacamayo("Lolo", 2, 1.4, "");
+        
+        
+        
         MonoAullador moncholo = new MonoAullador("Moncholo", 5, 16.2, "Alouatta caraya");
         MonoAullador peporro = new MonoAullador("Peporro", 3, 15.0, "Alouatta caraya");
 
@@ -21,13 +23,17 @@ public class PrimerClaseVirtual {
         titi.moverse();
         System.out.println(titi.getTipoHabitat());
 
+        
+        //METODOS INDIVIDUALES
         if (titi instanceof SelvaTropical) {
-            titi.cazar(dumbo);
-            titi.protegerseDeDepredador(dumbo);
+            titi.cazar(perez);
+            titi.protegerseDeDepredador(lolo);
             titi.descansar();
             titi.competirPorAlimento(moncholo);
             titi.compartirAlimento(peporro);
         }
+        
+        //INTERACCIONES
 
     }
 }
